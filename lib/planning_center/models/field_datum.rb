65b9acf9789d6a252f1dcd4c87ae27a179699499
Isaiah Rogers <isaiah@mortarstone.com>
2022-04-06 12:36:37 -0700

    Add other models

---
+++ lib/planning_center/models/field_datum.rb
@@ -0,0 +1,48 @@
+module PlanningCenter
+  class FieldDatum < Base
+    IMMUTABLE_FIELDS = %i[id file file_content_type file_name file_size
+      person_id].freeze
+    FIELDS = %i[field_definition_id value].freeze
+
+    attribute :id, :integer
+    attribute :field_definition_id, :integer
+    attribute :file, :string
+    attribute :file_content_type, :string
+    attribute :file_name, :string
+    attribute :file_size, :integer
+    attribute :person_id, :integer
+    attribute :value, :string
+
+    FIELDS.each do |attr|
+      define_method "#{attr}=" do |value|
+        public_send "#{attr}_will_change!"
+        super(value)
+      end
+    end
+
+    define_attribute_methods *FIELDS
+
+    validates :field_definition_id, :person_id, presence: true
+
+    # belongs_to :person
+    # belongs_to :field_definition
+
+    def field_definition
+      FieldDefinition.find(field_definition_id)
+    end
+
+    def person
+      Person.find(person_id)
+    end
+
+    private
+
+    def endpoint
+      if persisted?
+        "#{base_endpoint}/#{id}"
+      else
+        "people/v2/people/#{person_id}/field_data"
+      end
+    end
+  end
+end
---
+++ lib/planning_center/models/person.rb
@@ -0,0 +1,62 @@
+module PlanningCenter
+  class Person < Base
+    IMMUTABLE_FIELDS = %i[id can_create_forms created_at demographic_avatar_url
+      directory_status name passed_background_check school_type updated_at].freeze
+    FIELDS = %i[accounting_administrator anniversary avatar birthdate child
+      first_name gender given_name grade graduation_year inactivated_at last_name
+      medical_notes membership middle_name nickname people_permissions
+      primary_campus_id remote_id site_administrator status].freeze
+
+    attribute :id, :integer
+    attribute :accounting_administrator, :boolean
+    attribute :anniversary, :date
+    attribute :avatar, :string
+    attribute :birthdate, :date
+    attribute :can_create_forms, :boolean
+    attribute :child, :boolean
+    attribute :created_at, :datetime
+    attribute :demographic_avatar_url, :string
+    attribute :directory_status, :string
+    attribute :first_name, :string
+    attribute :gender, :string
+    attribute :given_name, :string
+    attribute :grade, :string
+    attribute :graduation_year, :integer
+    attribute :inactivated_at, :datetime
+    attribute :last_name, :string
+    attribute :medical_notes, :string
+    attribute :membership, :string
+    attribute :middle_name, :string
+    attribute :name, :string
+    attribute :nickname, :string
+    attribute :passed_background_check, :boolean
+    attribute :people_permissions, :string
+    attribute :primary_campus_id, :integer
+    attribute :remote_id, :integer
+    attribute :school_type, :string
+    attribute :site_administrator, :boolean
+    attribute :status, :string
+    attribute :updated_at, :datetime
+
+    FIELDS.each do |attr|
+      define_method "#{attr}=" do |value|
+        public_send "#{attr}_will_change!"
+        super(value)
+      end
+    end
+
+    define_attribute_methods *FIELDS
+
+    validates :first_name, :last_name, presence: true
+
+    def field_data(params = {})
+      params = { where: params }
+      response = client.get("people/v2/people/#{id}/field_data", params).dig('data')
+
+      response.map do |hsh|
+        attrs = FieldDatum.format_response(hsh)
+        FieldDatum.new attrs, &:persist!
+      end
+    end
+  end
+end
