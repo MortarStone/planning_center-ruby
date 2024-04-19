e7c7ee3e1f712c4dfb5ae7defc1fded52c04c0a4
Deb Kallina <dkallina@gmail.com>
2024-04-19 10:01:05 -0400

    Add specs for more endpoints

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'vcr'
 require 'pry'
 require 'dotenv/load'
 
+Dir[File.join(File.dirname(__FILE__), '/support/**/*.rb')].each { |f| require f }
+
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock, :faraday
