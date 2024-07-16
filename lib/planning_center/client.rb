b5f72a291451d63c27a02c88485dab8a922c308b
Deb Kallina <dkallina@gmail.com>
2024-07-16 19:39:50 -0400

    Update ruby version

--- lib/planning_center/client.rb
+++ lib/planning_center/client.rb
@@ -69,7 +69,7 @@ module PlanningCenter
         req.params = params
         req.body = body
       end
-      # puts "#{method.upcase} #{response.env.url} #{params}\n\n"
+      puts "#{method.upcase} #{response.env.url} #{params}\n\n"
 
       ResponseHandler.new(response).call
     end
